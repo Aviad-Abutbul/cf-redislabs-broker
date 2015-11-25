e4f3624a9274535f5fa40431a9b60895c42bf439
Lev Berman <ldmberman@gmail.com>
2015-11-25 12:45:33 +0300

    Add basic service plan configuration

--- redislabs/config.go
+++ redislabs/config.go
@@ -1,4 +1,21 @@
 package redislabs
 
+import "github.com/pivotal-cf/brokerapi"
+
 type Config struct {
+	DefaultPlans []ServicePlanConfig `yaml:"plans"`
+}
+
+type ServicePlanConfig struct {
+	ID          string `yaml:"id"`
+	Name        string
+	Description string
+}
+
+func LoadServicePlan(p ServicePlanConfig) brokerapi.ServicePlan {
+	return brokerapi.ServicePlan{
+		ID:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+	}
 }
