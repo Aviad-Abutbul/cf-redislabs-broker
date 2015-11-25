f86029bfe1ff566e6d8fd9085a39397613fb975f
Lev Berman <ldmberman@gmail.com>
2015-11-25 14:14:58 +0300

    Wrap the test with a proper context

--- redislabs/broker_test.go
+++ redislabs/broker_test.go
@@ -2,7 +2,6 @@ package redislabs_test
 
 import (
 	"github.com/altoros/redislabs-service-broker/redislabs"
-	"github.com/pivotal-cf/brokerapi"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -10,31 +9,33 @@ import (
 
 var _ = Describe("Broker", func() {
 	var (
-		broker   redislabs.ServiceBroker
-		services []brokerapi.Service
-		config   redislabs.Config
+		broker redislabs.ServiceBroker
+		config redislabs.Config
 	)
 
-	BeforeEach(func() {
-		config = redislabs.Config{
-			DefaultPlans: []redislabs.ServicePlanConfig{
-				{
-					ID:          "",
-					Name:        "test",
-					Description: "",
-				},
-			},
-		}
+	JustBeforeEach(func() {
 		broker = redislabs.ServiceBroker{
 			Config: config,
 		}
-		services = broker.Services()
 	})
 
 	Describe("Looking for plans", func() {
-		It("Offers a service with at least one plan to use", func() {
-			Expect(len(services)).To(Equal(1))
-			Expect(len(services[0].Plans)).ToNot(Equal(0))
+		Context("Given a config with one default plan", func() {
+			BeforeEach(func() {
+				config = redislabs.Config{
+					DefaultPlans: []redislabs.ServicePlanConfig{
+						{
+							ID:          "",
+							Name:        "test",
+							Description: "",
+						},
+					},
+				}
+			})
+			It("Offers a service with at least one plan to use", func() {
+				Expect(len(broker.Services())).To(Equal(1))
+				Expect(len(broker.Services()[0].Plans)).ToNot(Equal(0))
+			})
 		})
 	})
 })
