aa425ab9c5497f01046cd0d5a049a3570dab7ee6
Lev Berman <ldmberman@gmail.com>
2015-11-25 14:17:51 +0300

    Add the very basic provisioning expectations

--- redislabs/broker_test.go
+++ redislabs/broker_test.go
@@ -2,6 +2,7 @@ package redislabs_test
 
 import (
 	"github.com/altoros/redislabs-service-broker/redislabs"
+	"github.com/altoros/redislabs-service-broker/vendor/github.com/pivotal-cf/brokerapi"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -35,6 +36,61 @@ var _ = Describe("Broker", func() {
 			It("Offers a service with at least one plan to use", func() {
 				Expect(len(broker.Services())).To(Equal(1))
 				Expect(len(broker.Services()[0].Plans)).ToNot(Equal(0))
+			})
+		})
+	})
+
+	Describe("Provisioning an instance", func() {
+		var (
+			serviceID, requestedServiceID, planID, requestedPlanID string
+			details                                                brokerapi.ProvisionDetails
+		)
+		Context("Given a config with a default plan", func() {
+			BeforeEach(func() {
+				config = redislabs.Config{
+					ID: serviceID,
+					DefaultPlans: []redislabs.ServicePlanConfig{
+						{
+							ID:          planID,
+							Name:        "test",
+							Description: "",
+						},
+					},
+				}
+				details = brokerapi.ProvisionDetails{
+					ID:               requestedServiceID,
+					PlanID:           requestedPlanID,
+					OrganizationGUID: "",
+					SpaceGUID:        "",
+				}
+			})
+			Context("And a wrong service ID", func() {
+				BeforeEach(func() {
+					requestedServiceID = "unknown"
+				})
+				It("Rejects to create an instance", func() {
+					err := broker.Provision("some-id", details)
+					Expect(err).To(HaveOccurred())
+				})
+			})
+			Context("And a wrong plan ID", func() {
+				BeforeEach(func() {
+					requestedServiceID = "unknown"
+				})
+				It("Rejects to create an instance", func() {
+					err := broker.Provision("some-id", details)
+					Expect(err).To(HaveOccurred())
+				})
+			})
+			Context("And given correct plan and instance IDs", func() {
+				BeforeEach(func() {
+					requestedServiceID = serviceID
+					requestedPlanID = planID
+				})
+				It("Creates an instance of the configured default plan", func() {
+					err := broker.Provision("some-id", details)
+					Expect(err).ToNot(HaveOccurred())
+				})
 			})
 		})
 	})
