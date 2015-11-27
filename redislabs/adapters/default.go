d0c0a6eb5c501d38e1908638e0899a147920f181
Lev Berman <ldmberman@gmail.com>
2015-11-27 13:52:42 +0300

    Write up a default creator Create skeleton

--- redislabs/adapters/default.go
+++ redislabs/adapters/default.go
@@ -1,13 +1,35 @@
 package adapters
 
 import (
+	"errors"
+	"sync"
+
 	"github.com/altoros/cf-redislabs-broker/redislabs/cluster"
 	"github.com/altoros/cf-redislabs-broker/redislabs/persisters"
 )
 
-type DefaultCreator struct{}
+type DefaultCreator struct {
+	sync.Mutex
+}
 
 func (d *DefaultCreator) Create(instanceID string, settings cluster.InstanceSettings, persister persisters.StatePersister) error {
+	d.Lock()
+	defer d.Unlock()
+
+	state, err := persister.Load()
+	if err != nil {
+		return errors.New("") // TODO
+	}
+
+	// check if instance exists
+
+	// make an API request
+
+	// update state
+
+	if err = persister.Save(state); err != nil {
+		return errors.New("") // TODO
+	}
 	return nil
 }
 
