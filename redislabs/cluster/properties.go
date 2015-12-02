8c327b220850d2bfc1bcb45443c5a37c5a9355b1
Lev Berman <ldmberman@gmail.com>
2015-12-02 17:52:42 +0300

    Include a password into instance settings

--- redislabs/cluster/properties.go
+++ redislabs/cluster/properties.go
@@ -3,9 +3,10 @@ package cluster
 // InstanceSettings is a JSON serializable collection of properties necessary
 // for the creation of a cluster instance (database).
 type InstanceSettings struct {
-	MemoryLimit int64 `json:"memory_size"`
-	Replication bool  `json:"replication"`
-	ShardCount  int64 `json:"shards_count"`
+	MemoryLimit int64  `json:"memory_size"`
+	Replication bool   `json:"replication"`
+	ShardCount  int64  `json:"shards_count"`
+	Password    string `json:"authentication_redis_pass"`
 }
 
 // InstanceCredentials contains properties necessary for identifying a
