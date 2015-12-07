f355920d02ed527b1bf3faddb1d5661fa54d7759
Lev Berman <ldmberman@gmail.com>
2015-12-07 13:15:34 +0300

    Clean up things

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -3,23 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	"os"
+	"path"
 
 	"github.com/Altoros/cf-redislabs-broker/redislabs"
 	"github.com/Altoros/cf-redislabs-broker/redislabs/config"
 	"github.com/Altoros/cf-redislabs-broker/redislabs/instance_binders"
 	"github.com/Altoros/cf-redislabs-broker/redislabs/instance_creators"
 	"github.com/Altoros/cf-redislabs-broker/redislabs/persisters"
-	// "github.com/Altoros/cf-redislabs-broker/redislabs/persisters"
-	"net/http"
-	"os"
-
 	"github.com/ldmberman/brokerapi"
 	"github.com/pivotal-golang/lager"
 )
 
 var (
+	localPersisterPath = path.Join(os.Getenv("HOME"), ".redislabs-broker", "state.json")
+
 	brokerConfigPath string
-	brokerPidFile    string
 )
 
 func init() {
@@ -29,7 +29,6 @@ func init() {
 }
 
 func main() {
-
 	brokerLogger := lager.NewLogger("redislabs-service-broker")
 	brokerLogger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
 	brokerLogger.RegisterSink(lager.NewWriterSink(os.Stderr, lager.ERROR))
@@ -39,32 +38,28 @@ func main() {
 	}
 
 	brokerLogger.Info("Config File: " + brokerConfigPath)
-	config, err := config.LoadFromFile(brokerConfigPath)
 
+	conf, err := config.LoadFromFile(brokerConfigPath)
 	if err != nil {
 		brokerLogger.Fatal("Loading config file", err, lager.Data{
 			"broker-config-path": brokerConfigPath,
 		})
 	}
 
-	// instanceCreator := redislabs.ServiceInstanceCreator{}
-	// instanceBinder := redislabs.ServiceInstanceBinder{}
-	// persister := persisters.StatePersister{}
-
 	serviceBroker := redislabs.NewServiceBroker(
-		instancecreators.NewDefault(config, brokerLogger),
-		instancebinders.NewDefault(config, brokerLogger),
-		persisters.NewLocalPersister("state.json"),
-		config,
+		instancecreators.NewDefault(conf, brokerLogger),
+		instancebinders.NewDefault(conf, brokerLogger),
+		persisters.NewLocalPersister(localPersisterPath),
+		conf,
 		brokerLogger,
 	)
 
 	credentials := brokerapi.BrokerCredentials{
-		Username: config.ServiceBroker.Auth.Username,
-		Password: config.ServiceBroker.Auth.Password,
+		Username: conf.ServiceBroker.Auth.Username,
+		Password: conf.ServiceBroker.Auth.Password,
 	}
 
 	brokerAPI := brokerapi.New(serviceBroker, brokerLogger, credentials)
 	http.Handle("/", brokerAPI)
-	http.ListenAndServe(fmt.Sprintf(":%s", config.ServiceBroker.Port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", conf.ServiceBroker.Port), nil)
 }
