88132a522c05c8e7f2f6587c6a21a6d5ad28f46a
Lev Berman <ldmberman@gmail.com>
2015-12-09 15:15:32 +0300

    Fix the ill implementation of the password generator

--- redislabs/passwords/generate.go
+++ redislabs/passwords/generate.go
@@ -13,8 +13,9 @@ var (
 // Generate returns a randomly generated password of the given strength (length).
 // Password characters are taken from the passwords.Characters set.
 func Generate(length int) (string, error) {
-	pass := make([]byte, length)
+	pass := []byte{}
 
+	min, max := minMax(Characters)
 	for len(pass) < length {
 		chunk := make([]byte, length)
 		_, err := io.ReadFull(rand.Reader, chunk)
@@ -22,11 +23,29 @@ func Generate(length int) (string, error) {
 			return "", err
 		}
 		for _, c := range chunk {
-			if c >= byte(len(Characters)) {
+			if c > max || c < min {
 				continue
 			}
-			pass = append(pass, Characters[c%byte(len(Characters))])
+			pass = append(pass, c)
 		}
 	}
 	return string(pass), nil
+}
+
+func minMax(bytes []byte) (byte, byte) {
+	if len(bytes) == 0 {
+		panic("minMax has been called with an empty byte array")
+	}
+	var (
+		min = bytes[0]
+		max = bytes[0]
+	)
+	for _, b := range bytes {
+		if b < min {
+			min = b
+		} else if b > max {
+			max = b
+		}
+	}
+	return min, max
 }
